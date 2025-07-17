32d424422b39463004f925a1e965ffb4ac2de4e6
AmandaLeopoldoo <amandaleopoldo2930@gmail.com>
2025-07-17 19:56:29 -0300

    Feat: Criação dos contrutores e getter,setters da classe e metodo.

---
+++ src/entities/ItemsPedido.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class ItemsPedido {
+
+    private Integer quantidade;
+    private Double preco;
+
+    public ItemsPedido() {
+    }
+
+    public ItemsPedido(Integer quantidade, Double preco) {
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public double subTotal(){
+        return quantidade * preco;
+    }
+}
